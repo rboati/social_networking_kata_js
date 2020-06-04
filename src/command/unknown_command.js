cddca03cd7010826b565433765012938818c1b5c
Roberto Boati <3063860+rboati@users.noreply.github.com>
2020-06-04 20:35:28 +0100

    Review

--- src/command/unknown_command.js
+++ src/command/unknown_command.js
@@ -1,7 +1,4 @@
-import assert from 'assert';
 import { ICommand } from './command_interface.js';
-import { IConsole } from '../ui/console_interface.js';
-
 
 export class UnknownCommand extends ICommand {
 	execute(userName, ...args) {
